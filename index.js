c6273944758d16f146a42a49be415f543e5df0d4
jojusun <90098233+jojusun@users.noreply.github.com>
2021-12-03 16:27:31 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -2,35 +2,70 @@
 function App() {
     
    const $ = (selector) => document.querySelector(selector);
-   
-   $('.input-todo').addEventListener('keypress',(e) =>{
-       if(e.key==='Enter'){
-        const addTodoList = $('.input-todo').value;
-        const todoitem = (addTodoList) => {
-            return `<div class = 'todo-list'>
-            <div class = 'add-todo-list'>
-                <div class = 'add-list'>${addTodoList}</div>
-                <button class = 'b1'>완료</button>
-                <button class = 'b2'>삭제</button>
-            </div>
-        </div>`
-        };
-        $('.real-last').insertAdjacentHTML('afterbegin',todoitem(addTodoList));
-   }
-})
+   const addTodoList = $('.input-todo').value;
+    //add html tag
+   const todoItem = (addTodoList) => {
+    return `<div class = 'todo-list'>
+                <div class = 'add-todo-list'>
+                   <div class = 'add-list'>${addTodoList}</div>
+                   <button class = 'b1'>완료</button>
+                   <button class = 'b2'>삭제</button>
+                </div>
+            </div>`
+    };
 
+    const ok = () => {
+        return ` <div class = 'todo-list'>
+        <div class = 'add-todo-list'>
+           <div class = 'add-list'>완료되었습니다.</div>
+           <button class = 'b2' padding-left :320px;>삭제</button>
+        </div>
+    </div>`
+    }
+   
+    //enter눌렀을 때 목록 추가
+   $('.input-todo').addEventListener('keypress',(e) =>{
+ 
+     if(e.key ==='Enter'){
+         const addTodoList = $('.input-todo').value;
+         if(addTodoList!==''){
+        todoItem(addTodoList);
+        $('.real-last').insertAdjacentHTML('afterbegin',todoItem(addTodoList));
+        $('.input-todo').value = '';
+         } else if(addTodoList==='') {
+             alert('할일을 입력하세요');
+             return;
+         }
+       }
+    })
+
+    //버튼 눌렀을 때 목록 추가
     $('.add-button').addEventListener('click',(e) => {
         const addTodoList = $('.input-todo').value;
-        const todoitem = (addTodoList) => {
-            return `<div class = 'todo-list'>
-            <div class = 'add-todo-list'>
-                <div class = 'add-list'>${addTodoList}</div>
-                <button class = 'b1'>완료</button>
-                <button class = 'b2'>삭제</button>
-            </div>
-        </div>`
-        };
-        $('.real-last').insertAdjacentHTML('afterbegin',todoitem(addTodoList));
+        if(addTodoList!==''){
+        todoItem(addTodoList);
+        $('.real-last').insertAdjacentHTML('afterbegin',todoItem(addTodoList));
+        $('.input-todo').value = '';
+        } else if(addTodoList===''){
+            return alert('값을 입력하세요');
+        }
+    })
+
+    //삭제 버튼 
+    $('.real-last').addEventListener('click',(e) => {
+        if(e.target.classList.contains('b2')){
+            e.target.closest('.todo-list').remove();
+        }
+    })
+
+    //완료 버튼
+    $('.real-last').addEventListener('click',(e) => {
+        const addTodoList = $('.input-todo').value;
+
+        if(e.target.classList.contains('b1')){
+            e.target.closest('.add-todo-list').outerHTML =
+            ok(addTodoList);
+        }
     })
 }
 
